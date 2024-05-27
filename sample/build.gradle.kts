1af363874e65bfdad2a637a15136427c894fd0e7
dyguests <dyguests@qq.com>
2024-05-27 16:53:41 +0800

    add jitpack implementation

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -47,7 +47,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,8 +55,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":library"))
     implementation(libs.androidx.navigation.compose)
+    implementation(project(":library"))
+    // implementation("com.github.dyguests:LinCalendar:0.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
